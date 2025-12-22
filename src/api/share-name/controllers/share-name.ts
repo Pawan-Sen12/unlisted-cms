88a1b1b0542a3919eb490f4e55925088ee3f8579
pricnitz <anitesh123.na@gmail.com>
2025-12-22 12:12:15 +0530

    strapi cms

---
+++ src/api/share-name/controllers/share-name.ts
@@ -0,0 +1,7 @@
+/**
+ * share-name controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::share-name.share-name');
---
+++ src/api/share-name/routes/share-name.ts
@@ -0,0 +1,7 @@
+/**
+ * share-name router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::share-name.share-name');
